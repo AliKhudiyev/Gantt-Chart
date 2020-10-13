04ddfd1062908d926c82f3d5235849a58ad2ce35
AliKhudiyev <xudiyev-ali@mail.ru>
2020-10-13 19:13:04 +0400

    minor updates

--- main.py
+++ main.py
@@ -3,7 +3,57 @@ from app import *
 app = App()
 app.run()
 
-# text = "'a\nb'"
-#
-# print(text)
-# print(repr(text))
+'''
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+root = Tk()
+root.wm_title("Embedding in Tk")
+
+fig, ax = plt.subplots(figsize=(5, 4), dpi=100)
+
+# = = = = = = = = =
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+# = = = = = = = = =
+
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+canvas.draw()
+canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+
+toolbar = NavigationToolbar2Tk(canvas, root)
+toolbar.update()
+canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+
+
+def on_key_press(event):
+    print("you pressed {}".format(event.key))
+    key_press_handler(event, canvas, toolbar)
+
+
+canvas.mpl_connect("key_press_event", on_key_press)
+
+
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+
+button = Button(master=root, text="Quit", command=_quit)
+button.pack(side=BOTTOM)
+
+root.mainloop()
+'''
